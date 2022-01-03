ba7c74ce0690e538f14c8529b2e093f42613d58a
thomas <thomas.drege@testapic.com>
2022-01-03 15:30:49 +0100

    review all repositroy proposal

--- src/app/infrastructure/repository/backoffice/proposal/create.proposal.repository.ts
+++ src/app/infrastructure/repository/backoffice/proposal/create.proposal.repository.ts
@@ -1,6 +1,9 @@
 import {CreateProposalGateway} from "@app/domain/backoffice/gateway/proposal/create.proposal.gateway";
 import Proposal from "@app/infrastructure/model/proposal";
 import {Injectable} from "@nestjs/common";
+import ProposalStatus from "@app/infrastructure/model/proposal.status";
+import User from "@app/infrastructure/model/user";
+import Category from "@app/infrastructure/model/category";
 
 @Injectable()
 export class CreateProposalRepository implements CreateProposalGateway {
@@ -20,7 +23,28 @@ export class CreateProposalRepository implements CreateProposalGateway {
     }
 
     findProposalById(proposalId: number): Promise<Proposal> {
-        return Proposal.findByPk(proposalId);
+        return Proposal.findByPk(proposalId, {
+            include: [
+                {
+                    model: ProposalStatus,
+                    attributes: ['id', 'value']
+                },
+                {
+                    model: User,
+                    attributes: ['id', 'firstname', 'lastname', 'username']
+                },
+                {
+                    model: Category,
+                    attributes: ['id', 'value', 'color'],
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['id', 'firstname', 'lastname', 'username']
+                        }
+                    ]
+                }
+            ],
+        });
     }
 
 }
--- src/app/infrastructure/repository/backoffice/proposal/update.proposal.repository.ts
+++ src/app/infrastructure/repository/backoffice/proposal/update.proposal.repository.ts
@@ -1,12 +1,36 @@
 import {UpdateProposalGateway} from "@app/domain/backoffice/gateway/proposal/update.proposal.gateway";
 import Proposal from "@app/infrastructure/model/proposal";
 import {Injectable} from "@nestjs/common";
+import ProposalStatus from "@app/infrastructure/model/proposal.status";
+import User from "@app/infrastructure/model/user";
+import Category from "@app/infrastructure/model/category";
 
 @Injectable()
 export class UpdateProposalRepository implements UpdateProposalGateway {
 
     findProposalById(proposalId: number): Promise<Proposal> {
-        return Proposal.findByPk(proposalId);
+        return Proposal.findByPk(proposalId, {
+            include: [
+                {
+                    model: ProposalStatus,
+                    attributes: ['id', 'value']
+                },
+                {
+                    model: User,
+                    attributes: ['id', 'firstname', 'lastname', 'username']
+                },
+                {
+                    model: Category,
+                    attributes: ['id', 'value', 'color'],
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['id', 'firstname', 'lastname', 'username']
+                        }
+                    ]
+                }
+            ],
+        });
     }
 
     updateProposal(proposalId: number, title: string, description: string, newspaperNumber: number, proposalStatusId: number, categoryId: number): Promise<[number, Proposal[]]> {
