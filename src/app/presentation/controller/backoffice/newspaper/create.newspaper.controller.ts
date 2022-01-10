135f4548728aa30ac29e11ae1ec64a586d75d679
drege <thomasdrege1@gmail.com>
2022-01-10 22:23:29 +0100

    review user

--- src/app/presentation/controller/backoffice/newspaper/create.newspaper.controller.ts
+++ src/app/presentation/controller/backoffice/newspaper/create.newspaper.controller.ts
@@ -1,10 +1,12 @@
-import {Controller, Inject, Post, Req, Res, UseGuards} from "@nestjs/common";
+import {Controller, Inject, Post, Req, Res, UploadedFile, UseGuards, UseInterceptors} from "@nestjs/common";
 import {JwtAuthGuard} from "@app/infrastructure/auth/jwt/jwt.guard";
 import {RolesGuard} from "@app/infrastructure/auth/roles/roles.guard";
 import {CreateNewspaper} from "@app/domain/backoffice/usecase/newspaper/create.newspaper";
 import {CreateNewspaperPresenterImpl} from "@app/domain/backoffice/presenter/newspaper/create.newspaper.presenter.impl";
 import {CreateNewspaperRequest} from "@app/domain/backoffice/request/newspaper/create.newspaper.request";
 import {CreateNewspaperViewModel} from "@app/presentation/viewmodel/backoffice/newspaper/create.newspaper.viewmodel";
+import {FileInterceptor} from "@nestjs/platform-express";
+import {diskStorage} from "multer";
 
 @Controller('/newspapers')
 export class CreateNewspaperController {
@@ -18,8 +20,16 @@ export class CreateNewspaperController {
     }
 
     @Post()
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    async createAction(@Req() req, @Res() res) {
+    //@UseGuards(JwtAuthGuard, RolesGuard)
+    @UseInterceptors(FileInterceptor('upload', {
+        storage: diskStorage({
+            destination: './public/image/upload',
+            filename: (req, file, cb) => {
+                cb(null, file.originalname)
+            }
+        })
+    }))
+    async createAction(@Req() req, @Res() res, @UploadedFile() file: Express.Multer.File) {
         const vm: CreateNewspaperViewModel = this.presenter.present(
             await this.usecase.execute(new CreateNewspaperRequest(
                 req.body.number,
